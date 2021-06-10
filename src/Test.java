234e4ad6e8bb18aa48b5bef96128b7234c99862c
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-10 22:39:40 +0300

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+        Dungeon dung = new Dungeon();
+        dung.rooms();
+    }
+}
---
+++ src/Townspeople.java
@@ -0,0 +1,2 @@
+public class Townspeople {
+}
