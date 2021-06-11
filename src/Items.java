b6d4ccb21cc13e3a47fef9123fdf9da423fd9bd6
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-11 21:15:53 +0300

    Update Items.java

--- src/Items.java
+++ src/Items.java
@@ -1,2 +1,58 @@
 public class Items {
+
+    private int weight;
+    private double value;
+    private int damage;
+    private int armorAmount;
+    private int range;
+
+    public Item(){}
+    public Item(int weight, double value, int damage,int armorAmount,int range) {
+        this.weight = weight;
+        this.value = value;
+        this.damage = damage;
+        this.armorAmount = armorAmount;
+        this.range = range;
+    }
+
+    public int getArmorAmount() {
+        return armorAmount;
+    }
+
+    public void setArmorAmount(int armorAmount) {
+        this.armorAmount = armorAmount;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+ 
 }
