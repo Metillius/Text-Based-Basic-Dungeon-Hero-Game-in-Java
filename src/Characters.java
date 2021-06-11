a5cc64786d406c570f7cc2f5a6b383eed598bd05
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-11 21:04:48 +0300

    Update Characters.java

--- src/Characters.java
+++ src/Characters.java
@@ -1,2 +1,32 @@
-public class Characters {
+public class Character {
+
+    private int health;
+    private String name;
+
+
+    public Character(){}
+    public Character(int health, String name) {
+        this.health = health;
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printCharacterInfo(){}
+
+ 
 }
