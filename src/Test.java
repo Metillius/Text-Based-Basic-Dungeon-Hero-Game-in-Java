8aec1b3088721f8ea6dfa00f99876e538ba4728e
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-13 18:18:28 +0300

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,25 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        
-         Hero hero = new Hero();
 
-        hero.printCharacterInfo();
+
+        Hero hero = new Hero(Weapon.Old_Sword());
+        Monster monster = new Monster(Weapon.The_Master_Sword());
+
+
+        hero.printCharacterInfo(hero);
+
+
+
+        System.out.println("***************************************");
+        monster.monsterStatus();
+        monster.printCharacterInfo(monster);
+
+
+
+
+
+
+
     }
-}
+ }
