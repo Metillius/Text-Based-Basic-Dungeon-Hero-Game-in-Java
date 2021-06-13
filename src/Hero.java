2cf7a802dde7d5595dae53f9aad179454e873654
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-13 18:17:01 +0300

    Update Hero.java

--- src/Hero.java
+++ src/Hero.java
@@ -1,27 +1,39 @@
+import java.util.Scanner;
+
 public class Hero extends Character {
 
-    Weapon The_Master_Sword;
+    Weapon weapon;
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
 
     Scanner scanner = new Scanner(System.in);
 
 
-    public Hero(){
+    public Hero(Weapon weapon){
         super();
+        this.weapon = weapon;
     }
 
-    @Override
-    public void printCharacterInfo(){
+
+    public void printCharacterInfo(Hero hero){
 
         System.out.println("(Please enter a name)");
          String tempname = scanner.nextLine();
         setName(tempname);
         System.out.println("My name is " + getName()+ " I am the hero of this dungeon!");
         setHealth(100);
-        System.out.println(getName()+" Health: "+getHealth());
+        System.out.println(getName());
+
+
+        System.out.println(" Health: "+getHealth() +" name: "+ hero.weapon.getName()+" weight: "+ hero.weapon.getWeight()+ " value: "+ hero.weapon.getValue()+ " damage: "+ hero.weapon.getDamage()+" range: "+hero.weapon.getRange());
 
 
     }
 
- 
-  
-}
+ }
