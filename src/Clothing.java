dc5f758e1bfc3f5bbd5b1746baab65461a3f535b
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-15 22:29:57 +0300

    Update Clothing.java

--- src/Clothing.java
+++ src/Clothing.java
@@ -1,2 +1,38 @@
 public class Clothing {
+  
+  public class Clothing extends Item {
+
+
+
+    public Clothing (String name, String rare){
+        super(name,rare);
+    }
+
+        public static Clothing Fabric_Armour(){
+            Clothing Fabric_Armour = new Clothing("Fabric Armour","Rare very Rare");
+            return Fabric_Armour;
+        }
+
+        public static Clothing Rigid_Armour() {
+            Clothing Rigid_Armour = new Clothing ("Rigid Armour","Common Rare");
+            return Rigid_Armour;
+        }
+
+        public static Clothing Horn_Armour(){
+            Clothing Horn_Armour = new Clothing("Horn Armour","Common Rare");
+            return Horn_Armour;
+        }
+
+        public static Clothing Wood_Armour(){
+            Clothing Wood_Armour = new Clothing("Wood Armour","Legendary Rare");
+            return Wood_Armour;
+        }
+
+        public static Clothing Plastic_Armour(){
+            Clothing Plastic_Armour = new Clothing("Plastic Armour","Legendary Rare");
+            return Plastic_Armour;
+        }
+
+}
+  
 }
