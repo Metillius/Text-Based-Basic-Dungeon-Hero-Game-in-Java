d2ac3bfa9b2770244250036d203a6704a674caed
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-15 23:11:33 +0300

    Update Townspeople.java

--- src/Townspeople.java
+++ src/Townspeople.java
@@ -1,2 +1,54 @@
+import java.util.Scanner;
 public class Townspeople {
-}
+
+
+
+        private int clothing,  damage, healthy, money;
+        private String name;
+        Scanner scanner;
+
+        //TODO some townspeople may heal the hero
+
+        {
+            scanner = new Scanner(System.in);
+        }
+
+        public void getClothing() {
+            this.name = name;
+        }
+
+        public void setClothing(int clothing) {
+            this.clothing = clothing;
+        }
+
+        public int getDamage() {
+            return damage;
+        }
+
+        public void setDamage(int damage) {
+            this.damage = damage;
+        }
+
+        public int getHealthy() {
+            return healthy;
+        }
+
+        public void setHealthy(int healthy) {
+            this.healthy = healthy;
+        }
+
+        public int getMoney() {
+            return money;
+        }
+
+        public void setMoney(int money) {
+            this.money = money;
+        }
+
+
+
+
+    }
+
+
+
