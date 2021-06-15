0aef7e8947149a03c001f0dea82e062ff4885ae4
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-15 22:41:44 +0300

    Update Clothing.java

--- src/Clothing.java
+++ src/Clothing.java
@@ -1,5 +1,4 @@
-
-  public class Clothing extends Item {
+public class Clothing extends Item {
 
 
 
@@ -7,31 +6,31 @@
         super(name,rare);
     }
 
-        public static Clothing Fabric_Armour(){
-            Clothing Fabric_Armour = new Clothing("Fabric Armour","Rare very Rare");
-            return Fabric_Armour;
-        }
+    public static Clothing Fabric_Armour(){
+        Clothing Fabric_Armour = new Clothing("Fabric Armour","Very Rare");
+        return Fabric_Armour;
+    }
 
-        public static Clothing Rigid_Armour() {
-            Clothing Rigid_Armour = new Clothing ("Rigid Armour","Common Rare");
-            return Rigid_Armour;
-        }
+    public static Clothing Rigid_Armour() {
+        Clothing Rigid_Armour = new Clothing ("Rigid Armour","Common");
+        return Rigid_Armour;
+    }
 
-        public static Clothing Horn_Armour(){
-            Clothing Horn_Armour = new Clothing("Horn Armour","Common Rare");
-            return Horn_Armour;
-        }
+    public static Clothing Horn_Armour(){
+        Clothing Horn_Armour = new Clothing("Horn Armour","Common");
+        return Horn_Armour;
+    }
 
-        public static Clothing Wood_Armour(){
-            Clothing Wood_Armour = new Clothing("Wood Armour","Legendary Rare");
-            return Wood_Armour;
-        }
+    public static Clothing Wood_Armour(){
+        Clothing Wood_Armour = new Clothing("Wood Armour","Legendary");
+        return Wood_Armour;
+    }
 
-        public static Clothing Plastic_Armour(){
-            Clothing Plastic_Armour = new Clothing("Plastic Armour","Legendary Rare");
-            return Plastic_Armour;
-        }
+    public static Clothing Plastic_Armour(){
+        Clothing Plastic_Armour = new Clothing("Plastic Armour","Legendary Rare");
+        return Plastic_Armour;
+    }
 
 }
-  
+
 
