f98fffba1e95fb5d8c9d9c5bcde14ad56c52803e
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-19 08:56:56 +0300

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,8 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Test {
     public static void main(String[] args) {
 
         Game game = new Game();
-        Scanner scanner = new Scanner(System.in);
-
         game.fighting();
+
     }
 }
