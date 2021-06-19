e66b75aeb3d533de694743249402aa2018d89ec2
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-19 08:59:19 +0300

    Create GameMechanics.java

---
+++ src/GameMechanics.java
@@ -0,0 +1,35 @@
+public interface GameMechanics {
+
+
+
+
+
+    //This method will be used to design all of the game and it is really important
+    void fighting();
+
+
+
+
+    String roomInfo = "There is an merciless Orc in this room";
+
+    String fightChoice = "Would you like to fight with this monster? yes/no ";
+
+    String normalLine = "______________________________________________________________________";
+
+    String  deathLine = "*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉*҉";
+    String fallenHero = "*҉*҉The hero of this Dungeon is fallen (ಥ﹏ಥ) *҉*҉";
+
+    String storageLine = "⚛___________________________________⚛";
+
+    String levelLine = "『-』『-』『-』 『-』 『-』 『-』『-』";
+    String nextLevel = "『-』 You are at the next level 『-』";
+
+
+
+
+    String gameOver = "\"\\n\" +\n" +
+            "                            \"▒█▀▀█ ░█▀▀█ ▒█▀▄▀█ ▒█▀▀▀ 　 ▒█▀▀▀█ ▒█░░▒█ ▒█▀▀▀ ▒█▀▀█ \\n\" +\n" +
+            "                            \"▒█░▄▄ ▒█▄▄█ ▒█▒█▒█ ▒█▀▀▀ 　 ▒█░░▒█ ░▒█▒█░ ▒█▀▀▀ ▒█▄▄▀ \\n\" +\n" +
+            "                            \"▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▄ 　 ▒█▄▄▄█ ░░▀▄▀░ ▒█▄▄▄ ▒█░▒█\"";
+
+}
