bdc0108a751e2f6e18f18d43b072d4aec7fba2eb
Metillius <85706449+Metillius@users.noreply.github.com>
2021-06-19 18:29:59 +0300

    Update Armor.java

--- src/Armor.java
+++ src/Armor.java
@@ -1,11 +1,14 @@
 public class Armor extends Item {
 
+    //armor is an item
+
 
 
     public Armor (String name, int protection, double value,int weight){
         super(name,protection,value,weight);
     }
 
+    //armor mantığı şöyle açıklanabilir ismi protectionu ve value su ve weighti var protection heroyu v monstrı hasardan koruyo ve verilen hasardan çıkarılıyo value hero alırsa scoruna ekleniyo weight de hero alırsa çantasında ağırlık oluşturmasını sağlıyo
     public static Armor leatherArmor(){
         Armor LeatherArmor = new Armor("Leather Armor",1 , 10,3);
         return LeatherArmor;
